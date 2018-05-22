b24a3f9fa617b4558e1d4a68060ff26d46e42260
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-05-23 05:41:03 +0700

    added alias path in webpack

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -90,6 +90,11 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
+      'src':path.resolve(__dirname,'../src'),
+      'components':path.resolve(__dirname,'../src/components'),
+      'styles':path.resolve(__dirname,'../src/styles'),
+      'actions':path.resolve(__dirname,'../src/store/actions'),
+      'images':path.resolve(__dirname,'../src/images'),
       
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
