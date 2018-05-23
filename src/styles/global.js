2dd8b6ba0e434e935fbe360772c5f703a791bd80
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-05-23 08:14:02 +0700

    added font-family

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,6 +2,7 @@ import { injectGlobal } from 'styled-components';
 import './globalReset'
 injectGlobal`
     body{
+        font-family: 'Helvetica, sans-serif';        
         background-color:hsla(290,60%,70%,0.3);
     }
 `
