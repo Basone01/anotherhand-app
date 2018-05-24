138517a735bd0b914bf43382097f3f738997bc94
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-05-25 01:14:00 +0700

    break nested css to styled components

--- src/components/inputBox.js
+++ src/components/inputBox.js
@@ -1,36 +1,56 @@
 import React from 'react'
 import styled from 'styled-components';
 
+const Label = styled.span `
+    display:flex;
+    flex-shrink:0;
+    align-items:center;
+    padding:14px 24px 12px 24px;
+`;
+
+const InputContainer = styled.span `
+    display:flex;
+    flex-grow:1;
+    
+`
+
+const Input = styled.input `
+    padding:14px 24px 12px 24px;
+    flex-grow:1;
+    min-width: 100%;
+    max-width: 100%;
+    width: 100%;
+    box-sizing:border-box;
+    border:none;
+    outline:none;
+    text-align:end;
+    &[type=number]::-webkit-inner-spin-button, 
+    &[type=number]::-webkit-outer-spin-button { 
+        -webkit-appearance: none; 
+        margin: 0; 
+    }
+`
+
 const InputBoxContainer = styled.label `
     display:flex;
-    font-size:16px;
+    font-size:14px;
     font-weight:bold;
     border-bottom:1px solid rgba(0,0,0,0.2);
-    &>input{
-        flex-grow:1;
-        flex-shrink:0;
-        padding:14px 24px 12px 24px;
-        border:none;
-        outline:none;
-        text-align:end;
-    }
-    &>span{
-        display:flex;
-        flex-shrink:0;
-        
-        align-items:center;
-        padding:14px 24px 12px 24px;
-    }    
+
 `;
 
 const InputBox = ({
     prefix,
+    type,
     ...rest
 }) => {
     return (
         <InputBoxContainer>
-            <span>{prefix}</span>
-            <input type="text" {...rest}/>
+            <Label>{prefix}</Label>
+            <InputContainer>
+                <Input type={type || "text"} onChange={rest.onChange} {...rest}/>
+            </InputContainer>
+
         </InputBoxContainer>
     )
 }
