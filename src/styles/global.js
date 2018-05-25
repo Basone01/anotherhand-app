e037168f144f31da2b6fa83facc1e3587ce3d186
kanazang <adongawa.kanapat@gmail.com>
2018-05-26 04:36:32 +0700

    add more global reset

--- src/styles/global.js
+++ src/styles/global.js
@@ -6,7 +6,7 @@ injectGlobal`
         font-family: 'Helvetica, sans-serif';        
         background-color:hsla(290,60%,70%,0.3);
     }
-    body,div,input,textarea,span,h1,h2,h3,h4,h5,h6,label,ul,li,ol{
+    body,div,input,textarea,span,h1,h2,h3,h4,h5,h6,label,ul,li,ol,image{
         box-sizing:border-box;
         margin:0;
         padding:0;
