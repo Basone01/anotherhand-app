dc9c88bb9cedcd2f3601a4a94fc68451856771a4
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-05-25 11:48:07 +0700

    change reset.css to narmalize.css

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,8 +1,15 @@
 import { injectGlobal } from 'styled-components';
-import './globalReset'
+import styledNormalize from 'styled-normalize';
 injectGlobal`
+    ${styledNormalize}
     body{
         font-family: 'Helvetica, sans-serif';        
         background-color:hsla(290,60%,70%,0.3);
     }
-`
+    body,div,input,textarea,span,h1,h2,h3,h4,h5,h6,label,ul,li,ol{
+        box-sizing:border-box;
+        margin:0;
+        padding:0;
+        
+    }
+`;
