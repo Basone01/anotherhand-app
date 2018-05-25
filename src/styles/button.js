c54a0ff8dd4ba1328e9b0ed30281dd268c340b1b
kanazang <adongawa.kanapat@gmail.com>
2018-05-26 06:55:10 +0700

    use base css

--- src/styles/button.js
+++ src/styles/button.js
@@ -1,14 +1,11 @@
 import styled from 'styled-components';
-
+import {basicButton} from './css'
 export const BlockButton = styled.a`
-	display: flex;
-	justify-content: center;
-	align-items: center;
+	${basicButton};
 	min-height: 36px;
 	background-color: white;
 	border: 1px solid rgba(0, 0, 0, 0.3);
 	border-style: dashed;
-	cursor: pointer;
 	transition: background 0.25s;
 	&:hover {
 		background-color: rgba(0, 0, 0, 0.1);
@@ -16,13 +13,9 @@ export const BlockButton = styled.a`
 `;
 
 export const IconButton = styled.a`
-	display: flex;
-	justify-content: center;
-	align-items: center;
+	${basicButton};
 	width: 32px;
 	height: 32px;
-	cursor: pointer;
-	user-select: none;
 	transition: color 0.25s;
 	&:hover {
 		color: white;
