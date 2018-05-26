923050d75c408097887174ca620b4af19327d611
kanazang <adongawa.kanapat@gmail.com>
2018-05-26 08:29:15 +0700

    added route params for edit

--- src/pages/addProduct.js
+++ src/pages/addProduct.js
@@ -1,221 +1,255 @@
-import { connect } from 'react-redux';
-import { createProduct } from 'actions';
-import BackButton from 'components/backButton';
-import ImageUploader from 'components/imageUploader';
-import InputBox from 'components/inputBox';
-import InputTextArea from 'components/inputTextArea';
-import React, { Component } from 'react';
-import SizeInput from 'components/sizeInput';
-import SubmitIcon from 'react-icons/lib/md/check';
-import TagsInput from 'components/tagsInput';
-
-import { BlockButton, IconButton } from '../styles/button';
-import { PageContainer, ScrollableContainer, TopNavbarContainer } from '../styles';
+import { connect } from "react-redux";
+import { createProduct } from "actions";
+import BackButton from "components/backButton";
+import ImageUploader from "components/imageUploader";
+import InputBox from "components/inputBox";
+import InputTextArea from "components/inputTextArea";
+import React, { Component } from "react";
+import SizeInput from "components/sizeInput";
+import SubmitIcon from "react-icons/lib/md/check";
+import TagsInput from "components/tagsInput";
+import { withRouter } from "react-router-dom";
+import { BlockButton, IconButton } from "../styles/button";
+import {
+  PageContainer,
+  ScrollableContainer,
+  TopNavbarContainer
+} from "../styles";
 
 export class AddProductPage extends Component {
-	state = {
-		images: [],
-		name: '',
-		price: 0,
-		stock: 0,
-		description: '',
-		tags: [],
-		sizes: [],
-		size_type: ''
-	};
-	constructor(props) {
-		super(props);
-		this.handleInputChange = this.handleInputChange.bind(this);
-		this.handleImageChange = this.handleImageChange.bind(this);
-		this.handleImageRemove = this.handleImageRemove.bind(this);
-		this.handleTagsChange = this.handleTagsChange.bind(this);
-		this.addSize = this.addSize.bind(this);
-		this.handleSizeRemove = this.handleSizeRemove.bind(this);
-		this.handleSizeChange = this.handleSizeChange.bind(this);
-		this.submit = this.submit.bind(this);
-		this.reset = this.reset.bind(this);
-	}
+  state = {
+    images: [],
+    name: "",
+    price: 0,
+    stock: 0,
+    description: "",
+    tags: [],
+    sizes: [],
+    size_type: ""
+  };
+  constructor(props) {
+    super(props);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleImageChange = this.handleImageChange.bind(this);
+    this.handleImageRemove = this.handleImageRemove.bind(this);
+    this.handleTagsChange = this.handleTagsChange.bind(this);
+    this.addSize = this.addSize.bind(this);
+    this.handleSizeRemove = this.handleSizeRemove.bind(this);
+    this.handleSizeChange = this.handleSizeChange.bind(this);
+    this.submit = this.submit.bind(this);
+    this.reset = this.reset.bind(this);
+  }
 
-	handleImageChange(newImage) {
-		this.setState(({ images }) => ({
-			images: [ ...images, newImage ]
-		}));
-	}
-	handleImageRemove(newImagesList) {
-		this.setState(({ images }) => ({ images: newImagesList }));
-	}
-	handleInputChange(e) {
-		this.setState({
-			[e.target.name]: e.target.value
-		});
-	}
+  componentDidMount = () => {
+    const { match, products } = this.props;
+    if (match.params.id) {
+      const {images_path,...rest} = products.find(
+        product => product._id === match.params.id
+      );
+	//   console.log(res)0
+	console.log(images_path)
+      this.setState(state => ({
+        ...rest,
+        images: images_path
+      }));
+    }
+  };
 
-	handleTagsChange(tagList) {
-		this.setState((state) => ({ tags: tagList }));
-	}
+  handleImageChange(newImage) {
+    this.setState(({ images }) => ({
+      images: [...images, newImage]
+    }));
+  }
+  handleImageRemove(newImagesList) {
+    this.setState(({ images }) => ({ images: newImagesList }));
+  }
+  handleInputChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
-	handleSizeRemove(index) {
-		this.setState(({ sizes }) => ({
-			sizes: sizes.filter((size, i) => i !== index)
-		}));
-	}
-	handleSizeChange(e, index) {
-		const { sizes } = this.state;
-		const updatedSize = {
-			...sizes[index],
-			[e.target.name]: e.target.value
-		};
-		this.setState(({ sizes }) => ({
-			sizes: [ ...sizes.slice(0, index), updatedSize, ...sizes.slice(index + 1) ]
-		}));
-	}
+  handleTagsChange(tagList) {
+    this.setState(state => ({ tags: tagList }));
+  }
 
-	addSize() {
-		this.setState(({ sizes, price }) => ({
-			sizes: [
-				...sizes,
-				{
-					size: '',
-					price: price,
-					stock: 0
-				}
-			]
-		}));
-	}
+  handleSizeRemove(index) {
+    this.setState(({ sizes }) => ({
+      sizes: sizes.filter((size, i) => i !== index)
+    }));
+  }
+  handleSizeChange(e, index) {
+    const { sizes } = this.state;
+    const updatedSize = {
+      ...sizes[index],
+      [e.target.name]: e.target.value
+    };
+    this.setState(({ sizes }) => ({
+      sizes: [...sizes.slice(0, index), updatedSize, ...sizes.slice(index + 1)]
+    }));
+  }
 
-	submit() {
-		const { sizes, images, name } = this.state;
-		let product = null;
-		if (name === '') {
-			console.error('Name is Required');
-			return;
-		}
-		if (images.length === 0) {
-			console.error('Image is Required');
-			return;
-		}
-		if (sizes.filter((size) => size.size !== '').length > 0) {
-			// console.error('Size is Required');
-			// return;
-			const { stock, ...rest } = this.state;
-			product = rest;
-		} else {
-			const { sizes, ...rest } = this.state;
-			product = rest;
-		}
-		this.props.createProduct(product);
-	}
+  addSize() {
+    this.setState(({ sizes, price }) => ({
+      sizes: [
+        ...sizes,
+        {
+          size: "",
+          price: price,
+          stock: 0
+        }
+      ]
+    }));
+  }
 
-	reset() {
-		this.setState((state) => ({
-			images: [],
-			name: '',
-			price: 0,
-			stock: 0,
-			description: '',
-			tags: [],
-			sizes: []
-		}));
-	}
+  submit() {
+    const { sizes, images, name } = this.state;
+    let product = null;
+    if (name === "") {
+      console.error("Name is Required");
+      return;
+    }
+    if (images.length === 0) {
+      console.error("Image is Required");
+      return;
+    }
+    if (sizes.filter(size => size.size !== "").length > 0) {
+      // console.error('Size is Required');
+      // return;
+      const { stock, ...rest } = this.state;
+      product = rest;
+    } else {
+      const { sizes, ...rest } = this.state;
+      product = rest;
+    }
+    this.props.createProduct(product);
+  }
 
-	render() {
-		const { images, description, tags, sizes, name, price, stock, size_type } = this.state;
-		return (
-			<PageContainer>
-				<TopNavbarContainer>
-					<BackButton />
-					<span
-						style={{
-							flexGrow: 1,
-							textAlign: 'center'
-						}}
-					>
-						เพิ่มสินค้าใหม่
-					</span>
-					<IconButton onClick={() => this.submit()}>
-						<SubmitIcon size={20} />
-					</IconButton>
-				</TopNavbarContainer>
-				<ScrollableContainer>
-					<ImageUploader
-						images={images}
-						onRemove={this.handleImageRemove}
-						onChange={this.handleImageChange}
-					/>
-					<InputBox
-						name="name"
-						value={name}
-						prefix="ชื่อ :"
-						placeholder="ใส่ชื่อสินค้าที่ต้องการ"
-						onChange={this.handleInputChange}
-					/>
-					<InputTextArea
-						name="description"
-						value={description}
-						prefix="รายละเอียด :"
-						placeholder="ใส่อะไรก็ใส่มาเหอะ"
-						onChange={this.handleInputChange}
-					/>
-					{/*only render when no size  */}
-					{!sizes.length && (
-						<InputBox
-							name="price"
-							value={price}
-							type="number"
-							min={0}
-							prefix="ราคา :"
-							placeholder="ใส่ราคาสินค้าที่ต้องการ"
-							onChange={this.handleInputChange}
-						/>
-					)}
-					{!sizes.length && (
-						<InputBox
-							name="stock"
-							value={stock}
-							type="number"
-							min={0}
-							prefix="จำนวน :"
-							placeholder="ใส่จำนวนสของสินค้า"
-							onChange={this.handleInputChange}
-						/>
-					)}
+  reset() {
+    this.setState(state => ({
+      images: [],
+      name: "",
+      price: 0,
+      stock: 0,
+      description: "",
+      tags: [],
+      sizes: []
+    }));
+  }
 
-					<TagsInput
-						tags={tags}
-						onChange={this.handleTagsChange}
-						prefix="แท็ก :"
-						placeholder="ใส่แท็กให้กับสินค้าของคุณ ไม่ว่าจะเป็น ยี่ห้อ รุ่น สี หรือประเภทของสินค้า เพื่อให้ง่ายต่อการค้นหา โดยกด Enter เพื่อเพิ่มแท็กแต่ละอัน"
-					/>
+  render() {
+    const {
+      images,
+      description,
+      tags,
+      sizes,
+      name,
+      price,
+      stock,
+      size_type
+    } = this.state;
+    return (
+      <PageContainer>
+        <TopNavbarContainer>
+          <BackButton />
+          <span
+            style={{
+              flexGrow: 1,
+              textAlign: "center"
+            }}
+          >
+            เพิ่มสินค้าใหม่
+          </span>
+          <IconButton onClick={() => this.submit()}>
+            <SubmitIcon size={20} />
+          </IconButton>
+        </TopNavbarContainer>
+        <ScrollableContainer>
+          <ImageUploader
+            images={images}
+            onRemove={this.handleImageRemove}
+            onChange={this.handleImageChange}
+          />
+          <InputBox
+            name="name"
+            value={name}
+            prefix="ชื่อ :"
+            placeholder="ใส่ชื่อสินค้าที่ต้องการ"
+            onChange={this.handleInputChange}
+          />
+          <InputTextArea
+            name="description"
+            value={description}
+            prefix="รายละเอียด :"
+            placeholder="ใส่อะไรก็ใส่มาเหอะ"
+            onChange={this.handleInputChange}
+          />
+          {/*only render when no size  */}
+          {!sizes.length && (
+            <InputBox
+              name="price"
+              value={price}
+              type="number"
+              min={0}
+              prefix="ราคา :"
+              placeholder="ใส่ราคาสินค้าที่ต้องการ"
+              onChange={this.handleInputChange}
+            />
+          )}
+          {!sizes.length && (
+            <InputBox
+              name="stock"
+              value={stock}
+              type="number"
+              min={0}
+              prefix="จำนวน :"
+              placeholder="ใส่จำนวนสของสินค้า"
+              onChange={this.handleInputChange}
+            />
+          )}
 
-					<BlockButton onClick={this.addSize}>+ เพิ่มตัวเลือกไซส์สินค้า</BlockButton>
-					{sizes.map((size, i) => (
-						<SizeInput
-							key={i}
-							{...size}
-							onClick={(e) => this.handleSizeRemove(i)}
-							onChange={(e) => this.handleSizeChange(e, i)}
-						/>
-					))}
-					{sizes.length > 0 && (
-						<InputBox
-							name="size_type"
-							value={size_type}
-							prefix="หน่วยไซส์ :"
-							placeholder="ใส่หน่วยให้กับสินค้าของท่าน (ถ้ามี)"
-							onChange={this.handleInputChange}
-						/>
-					)}
-					<BlockButton onClick={this.reset}>ล้างข้อมูล</BlockButton>
-				</ScrollableContainer>
-			</PageContainer>
-		);
-	}
+          <TagsInput
+            tags={tags}
+            onChange={this.handleTagsChange}
+            prefix="แท็ก :"
+            placeholder="ใส่แท็กให้กับสินค้าของคุณ ไม่ว่าจะเป็น ยี่ห้อ รุ่น สี หรือประเภทของสินค้า เพื่อให้ง่ายต่อการค้นหา โดยกด Enter เพื่อเพิ่มแท็กแต่ละอัน"
+          />
+
+          <BlockButton onClick={this.addSize}>
+            + เพิ่มตัวเลือกไซส์สินค้า
+          </BlockButton>
+          {sizes.map((size, i) => (
+            <SizeInput
+              key={i}
+              {...size}
+              onClick={e => this.handleSizeRemove(i)}
+              onChange={e => this.handleSizeChange(e, i)}
+            />
+          ))}
+          {sizes.length > 0 && (
+            <InputBox
+              name="size_type"
+              value={size_type}
+              prefix="หน่วยไซส์ :"
+              placeholder="ใส่หน่วยให้กับสินค้าของท่าน (ถ้ามี)"
+              onChange={this.handleInputChange}
+            />
+          )}
+          <BlockButton onClick={this.reset}>ล้างข้อมูล</BlockButton>
+        </ScrollableContainer>
+      </PageContainer>
+    );
+  }
 }
 
-const mapStateToProps = (state) => ({});
-
-const mapDispatchToProps = (dispatch) => ({
-	createProduct: (product) => dispatch(createProduct(product))
+const mapStateToProps = ({ product }) => ({
+  products: product.products
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(AddProductPage);
+const mapDispatchToProps = dispatch => ({
+  createProduct: product => dispatch(createProduct(product))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  withRouter(AddProductPage)
+);
