dcc12626c077f800abf60f994d8dc1c9f5670c34
kanazang <adongawa.kanapat@gmail.com>
2018-05-26 09:12:47 +0700

    added find product by id

--- src/api/product.js
+++ src/api/product.js
@@ -3,7 +3,9 @@ import {API_ENDPOINT} from 'src/config'
 export function getProducts() {
 	return axios.get(`${API_ENDPOINT}/products`);
 }
-
+export function getProductById(id) {
+	return axios.get(`${API_ENDPOINT}/product/${id}`);
+}
 export function createProduct(product) {
 	return axios.post(`${API_ENDPOINT}/product`, { ...product });
 }
