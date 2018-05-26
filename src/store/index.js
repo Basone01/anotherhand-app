179dff0b1535f8830ad40f5f6548db20890998ba
kanazang <adongawa.kanapat@gmail.com>
2018-05-27 04:14:47 +0700

    added redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,27 @@
-import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import reducers from './reducers';
-import rootSaga from './sagas';
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import logger from "redux-logger";
+import reducers from "./reducers";
+import rootSaga from "./sagas";
 
 export default function configureStore() {
-	const reduxSagaMiddleware = createSagaMiddleware();
-	const store = createStore(reducers, applyMiddleware(reduxSagaMiddleware));
+  const reduxSagaMiddleware = createSagaMiddleware();
+  const middlewares =
+    process.env.NODE_ENV === "development"
+      ? [reduxSagaMiddleware, logger]
+      : [reduxSagaMiddleware];
+  const store = createStore(reducers, applyMiddleware(...middlewares));
 
-	if (module.hot) {
-		// Enable Webpack hot module replacement for reducers
-		module.hot.accept('./reducers', () => {
-		  const nextRootReducer = require('./reducers/index');
-		  store.replaceReducer(nextRootReducer);
-		});
-	}
-	
-	return {
-		...store,
-		runSaga: () => reduxSagaMiddleware.run(rootSaga)
-	};
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept("./reducers", () => {
+      const nextRootReducer = require("./reducers/index");
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+
+  return {
+    ...store,
+    runSaga: () => reduxSagaMiddleware.run(rootSaga)
+  };
 }
