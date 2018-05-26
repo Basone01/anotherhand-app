bc9c0bc28c8c5cc9dc55ae9d1bfaab06ac45d616
kanazang <adongawa.kanapat@gmail.com>
2018-05-26 08:28:20 +0700

    remove redux

--- src/containerComponents/productList.js
+++ src/containerComponents/productList.js
@@ -1,25 +1,15 @@
 import React from "react";
-import { connect } from "react-redux";
 import { compose, withProps } from "recompose";
-import { getProducts } from "actions/product";
-import { bindActionCreators } from "redux";
 import ProductListItem from "components/productListItem";
 const enhance = compose(
-  connect(
-    ({ product: { products } }) => ({ products }),
-    dispatch => ({
-      fetch: bindActionCreators(getProducts, dispatch)
-    })
-  ),
   withProps(({ products }) => ({
-    productList: products.map((product) => (
+    productList: products.map(product => (
       <ProductListItem key={product._id} {...product} />
     ))
   }))
 );
-
-const ProductList = ({ productList }) => {
+const ProductList = enhance(({ productList }) => {
   return <ul>{productList}</ul>;
-};
+});
 
-export default enhance(ProductList);
+export default ProductList;
